04a156d83f3543425e9dd13bbed993a76443b9f5
moqie <s3mpau@gmail.com>
2022-11-22 18:53:47 +0300

    Added updating price

--- shop/views.py
+++ shop/views.py
@@ -17,7 +17,10 @@ def index(request):
 class PurchaseCreate(CreateView):
     model = Purchase
     fields = ['product', 'amount_order', 'person', 'address']
-
+    def update_price(self):
+        getprod = Product.objects.get(id=self.object.product.id)
+        if getprod.amount <= getprod.ordered_amount and getprod.flag == 0:
+            Product.objects.filter(id=self.object.product.id).update(price=self.object.product.price*1.12,flag=1)
     def form_valid(self, form):
         self.object = form.save()
         prods = Product.objects.get(id=self.object.product.id)
@@ -27,6 +30,7 @@ class PurchaseCreate(CreateView):
             Product.objects.filter(id=self.object.product.id).update(ordered_amount=prods.ordered_amount + \
                     self.object.amount_order)
             Product.objects.filter(id=self.object.product.id).update(amount=prods.amount - self.object.amount_order)
+            PurchaseCreate.update_price(self)
             return HttpResponse(f'Спасибо за покупку! <a href="/">Главная</a>')
 
 
